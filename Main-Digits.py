ed295c930903271cc81490e3f5e0027b56802578
julienmalka <julien.malka@epfl.ch>
2017-07-28 12:41:24 +0200

    Commentaires + python normes

--- Main-Digits.py
+++ Main-Digits.py
@@ -3,79 +3,51 @@ from mnist import MNIST
 from Image import *
 from random import shuffle
 import numpy as np
-NN = NeuralNetwork(28*28,10,30,3)
 
-mndata = MNIST('Data')
-images_data, labels_data = mndata.load_training()
-images_test, labels_test = mndata.load_testing()
+NN = NeuralNetwork(28 * 28, 10, 30, 10)
+data = MNIST('Data')
+images_data, labels_data = data.load_training()
+images_test, labels_test = data.load_testing()
 batch_size = 10
 epoch_number = 30
 images = []
 
 
-def vectorize(j):
+def to_vector(index):
     e = np.zeros((10, 1))
-    e[j] = 1.0
+    e[index] = 1.0
     return e
 
 
+def find_output(vector):
+    return np.argmax(vector)
 
-def findOutput(j):
-    return np.argmax(j)
-
-
-
-
-for o in range(0,len(images_data)):
-    images.append(Image(images_data[o],labels_data[o]))
-
-
-
-
+for o in range(0, len(images_data)):
+    images.append(Image(images_data[o], labels_data[o]))
 
 for j in range(0, epoch_number):
-    h=0
+    h = 0
     shuffle(images)
 
-    while h<len(images)-10:
+    while h < len(images) - 10:
 
-        for g in (h,h+batch_size):
+        for g in (h, h + batch_size):
+            image = np.array(images[g].image) / 255
+            """Training phase"""
+            NN.back_propagation(np.reshape(image, (28 * 28, 1)), to_vector(images[g].label))
 
-
-            image = np.array(images[g].image)/255
-
-            NN.backPropagation(np.reshape(image,(28*28,1)), vectorize(images[g].label))
-
-        NN.gradientDescent(batch_size)
+        NN.gradient_descent(batch_size)
         NN.clear()
         h = h + batch_size
 
-    count =0
+    count = 0
     for v in range(0, len(images_test)):
-        image = np.array(images_test[v])/255
 
+        """Testing phase"""
+        image = np.array(images_test[v]) / 255
 
+        if find_output(NN.propagate(np.reshape(image, (28 * 28, 1)))) == labels_test[v]:
+            count = count + 1
 
-        if findOutput(NN.propagate(np.reshape(image,(28*28,1))))==labels_test[v]:
-            count = count+1
-
-    image = np.array(images_test[0]) / 255
-    image = np.reshape(image,(28*28,1))
-
-
-
-
-    print("Epoch n°"+str(j+1)+" --> Réussis : "+ str(count)+"/10000 Making an accuracy of "+str(count*100/10000)+"%")
-
-
-
-
-
-
-
-
-
-
-
-
-
+    print("Epoch n°" + str(j + 1) + " --> Réussis : " + str(count) + "/10000 Making an accuracy of " + str(
+        count * 100 / 10000) + "%")
--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -1,130 +1,85 @@
 import numpy as matrix
+
+
 class NeuralNetwork:
-
-
-    def __init__(self, nbInput, nbOutput, nbNeurons, learningRate):
-        self.nbInput = nbInput
-        self.nbOutput = nbOutput
-        self.nbNeurons = nbNeurons
-        self.W1 = matrix.random.randn(nbNeurons,nbInput)
-        self.B1 = matrix.random.randn(nbNeurons,1)
-        self.W2 = matrix.random.randn(nbOutput,nbNeurons)
-        self.B2 = matrix.random.randn(nbOutput,1)
+    def __init__(self, nb_input, nb_output, nb_neurons, learning_rate):
+        self.nbInput = nb_input
+        self.nbOutput = nb_output
+        self.nbNeurons = nb_neurons
+        self.w1 = matrix.random.randn(nb_neurons, nb_input)
+        self.b1 = matrix.random.randn(nb_neurons, 1)
+        self.w2 = matrix.random.randn(nb_output, nb_neurons)
+        self.b2 = matrix.random.randn(nb_output, 1)
         self.errors = []
-        self.As = []
-        self.Zs = []
-        self.learningRate = learningRate
+        self.a_s = []
+        self.z_s = []
+        self.learning_rate = learning_rate
+        self.z1 = None
+        self.a1 = None
+        self.z2 = None
+        self.a2 = None
 
+    @staticmethod
+    def sigmoid(z):
+        return 1 / (1 + matrix.exp(-z))
 
-
-
-
-    def sigmoid(self, z):
-        return 1/(1+matrix.exp(-z))
-
-    def sigmoidPrime(self, z):
+    @staticmethod
+    def sigmoid_prime(z):
         return matrix.exp(-z) / ((1 + matrix.exp(-z)) ** 2)
 
+    def propagate(self, x):
+        """Method used to get an input x go through the neuronal network and outputs a2"""
+        self.z1 = matrix.dot(self.w1, x) + self.b1
+        self.a1 = self.sigmoid(self.z1)
+        self.z2 = matrix.dot(self.w2, self.a1) + self.b2
+        self.a2 = self.sigmoid(self.z2)
+        z = [self.z1, self.z2]
+        self.z_s.append(z)
+        a = [x, self.a1]
+        self.a_s.append(a)
+        return self.a2
 
-    def propagate(self, X):
-        self.Z1 = matrix.dot(self.W1,X) + self.B1
-        self.A1 = self.sigmoid(self.Z1)
-        self.Z2 = matrix.dot(self.W2, self.A1) + self.B2
-        self.A2 = self.sigmoid(self.Z2)
-        z = []
-        z.append(self.Z1)
-        z.append(self.Z2)
-        self.Zs.append(z)
-        a = []
-        a.append(X)
-        a.append(self.A1)
-        self.As.append(a)
-        return self.A2
-
-
-    def backPropagation(self, X, Y):
-
-        yHat = self.propagate(X)
-        #print(Y-yHat)
-
-        error_2 = matrix.multiply(-(Y - yHat),self.sigmoidPrime(self.Z2))
-        error_1 = (matrix.dot(self.W2.T, error_2) * self.sigmoidPrime(self.Z1))
-        error = [error_1,error_2]
+    def back_propagation(self, x, y):
+        """Calculate the errors vectors for a given vector"""
+        y_hat = self.propagate(x)
+        error_2 = matrix.multiply(-(y - y_hat), self.sigmoid_prime(self.z2))
+        error_1 = (matrix.dot(self.w2.T, error_2) * self.sigmoid_prime(self.z1))
+        error = [error_1, error_2]
         self.errors.append(error)
 
-
-
-
-    def gradientDescent(self, M):
-
+    def gradient_descent(self, m):
+        """Updates the bias and weights using the errors vectors of all
+        exemples that had been through backpropagation"""
         tampon = self.errors[0][0]
-        for i in range(0,len(self.errors)):
+        for i in range(1, len(self.errors)):
             tampon = tampon + self.errors[i][0]
 
-        tampon = tampon * self.learningRate/M
-        self.B1 = self.B1 - tampon
+        tampon = tampon * self.learning_rate / m
+        self.b1 = self.b1 - tampon
 
         tampon = self.errors[0][1]
-        for i in range(0, len(self.errors)):
+        for i in range(1, len(self.errors)):
             tampon = tampon + self.errors[i][1]
 
-        tampon = tampon * self.learningRate / M
+        tampon = tampon * self.learning_rate / m
 
-        self.B2 = self.B2 - tampon
-        tampon = matrix.dot(self.errors[0][0], matrix.transpose(self.As[0][0]))
-        for i in range(0, len(self.errors)):
-            tampon = tampon + matrix.dot(self.errors[i][0], matrix.transpose(self.As[i][0]))
+        self.b2 = self.b2 - tampon
+        tampon = matrix.dot(self.errors[0][0], matrix.transpose(self.a_s[0][0]))
+        for i in range(1, len(self.errors)):
+            tampon = tampon + matrix.dot(self.errors[i][0], matrix.transpose(self.a_s[i][0]))
 
-        tampon = tampon * self.learningRate / M
-        self.W1 = self.W1 - tampon
+        tampon = tampon * self.learning_rate / m
+        self.w1 = self.w1 - tampon
 
-        tampon = matrix.dot(self.errors[0][1], matrix.transpose(self.As[0][1]))
-        for i in range(0, len(self.errors)):
-            tampon = tampon + matrix.dot(self.errors[i][1], matrix.transpose(self.As[i][1]))
+        tampon = matrix.dot(self.errors[0][1], matrix.transpose(self.a_s[0][1]))
+        for i in range(1, len(self.errors)):
+            tampon = tampon + matrix.dot(self.errors[i][1], matrix.transpose(self.a_s[i][1]))
 
-        tampon = tampon * self.learningRate / M
-
-        self.W2 = self.W2 - tampon
-
+        tampon = tampon * self.learning_rate / m
+        self.w2 = self.w2 - tampon
 
     def clear(self):
+        """Clears the errors, as, and zs after a gradient descent"""
         self.errors.clear()
-        self.As.clear()
-        self.Zs.clear()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        self.a_s.clear()
+        self.z_s.clear()
