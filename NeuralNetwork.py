54a121c1fb68fadfb6bbec768b80f416174e76d1
julienmalka <julien.malka@epfl.ch>
2017-08-02 18:07:50 +0200

    Some bug fixed, still not fonctionnal

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -33,45 +33,47 @@ class NeuralNetwork:
     def propagate(self, x):
         """Method used to get an input x go through the neuronal network and outputs a2"""
         self.a.append(x)
-        self.z.append(matrix.dot(self.weights[0], x)+self.bias[0])
-        self.a.append(self.sigmoid(self.z[0]))
-        for i in range(1, self.size-1):
-            self.z.append(matrix.dot(self.weights[i], self.z[i-1]))
+        for i in range(0, self.size-1):
+            self.z.append(matrix.dot(self.weights[i], self.a[i]))
             self.a.append(self.sigmoid(self.z[i]))
         #self.z1 = matrix.dot(self.w1, x) + self.b1
         #self.a1 = self.sigmoid(self.z1)
         #self.z2 = matrix.dot(self.w2, self.a1) + self.b2
-        #self.a2 = self.sigmoid(self.z2)
-        z = [self.z1, self.z2]
-        self.z_s.append(self.z)
-        a = [x, self.a1]
-        self.a_s.append(self.a)
-        return self.a[len(self.a)-1]
+        #self.a2 = self.sigmo0id(self.z2)
+       # z = [self.z1, self.z2]
+        self.z_s.append(self.z.copy())
+
+        self.a_s.append(self.a.copy())
+        return self.a[-1]
 
     def back_propagation(self, x, y):
         """Calculate the errors vectors for a given vector"""
+
         error = [0] * (self.size-1)
         y_hat = self.propagate(x)
-        error[len(self.layers)-2] = matrix.multiply(-(y - y_hat), self.sigmoid_prime(self.z[len(self.z)-1]))
-        for i in range(1, len(self.layers)-1):
-            error[len(self.layers)-2-i] = matrix.dot(self.weights[len(self.layers)-1-i].T, error[len(self.layers)-1-i])*self.sigmoid_prime(self.z[len(self.layers)-2-i])
+        error[-1] = matrix.multiply(-(y - y_hat), self.sigmoid_prime(self.z[-1]))
+        for i in range(1, self.size-1):
+            error[-(i+1)] = matrix.dot(self.weights[-i].T, error[-i])*self.sigmoid_prime(self.z[-(i+1)])
 
 
         #error_1 = (matrix.dot(self.w2.T, error_2) * self.sigmoid_prime(self.z1))
         #error = [error_1, error_2]
         self.errors.append(error)
+        self.a.clear()
+        self.z.clear()
+
 
     def gradient_descent(self, m):
         """Updates the bias and weights using the errors vectors of all
         exemples that had been through backpropagation"""
-        for a in range(0, len(self.errors)):
+        for a in range(0, self.size-1):
             tampon = self.errors[0][a]
             for i in range(1, len(self.errors)):
                 tampon = tampon + self.errors[i][a]
             tampon = tampon * self.learning_rate / m
             self.bias[a] = self.bias[a] - tampon
 
-        for b in range(0, len(self.errors)):
+        for b in range(0, self.size-1):
             tampon = matrix.dot(self.errors[0][b], matrix.transpose(self.a_s[0][b]))
             for i in range(1, len(self.errors)):
                 tampon = tampon + matrix.dot(self.errors[i][b], matrix.transpose(self.a_s[i][b]))
