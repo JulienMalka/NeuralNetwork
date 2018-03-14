c1b0e453fba0796ae7571e8534c942bbb26ef231
Julien Malka <julien.malka@me.com>
2018-03-14 20:57:38 +0100

    Changed the name of numpy library in the code, it was unclear

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -1,4 +1,4 @@
-import numpy as matrix
+import numpy as np
 from random import shuffle
 from Exemple import *
 
@@ -11,9 +11,9 @@ class NeuralNetwork:
         self.weights = []
         self.bias = []
         for i in range(0, self.size - 1):
-            self.weights.append(matrix.random.randn(layers[i + 1], layers[i])/matrix.sqrt(layers[i+1]))
+            self.weights.append(np.random.randn(layers[i + 1], layers[i]) / np.sqrt(layers[i + 1]))
         for j in range(1, self.size):
-            self.bias.append(matrix.random.randn(layers[j], 1))
+            self.bias.append(np.random.randn(layers[j], 1))
         self.errors = []
         self.a_s = []
         self.z_s = []
@@ -22,21 +22,21 @@ class NeuralNetwork:
 
     @staticmethod
     def sigmoid(z):
-        return 1 / (1 + matrix.exp(-z))
+        return 1 / (1 + np.exp(-z))
 
     @staticmethod
     def sigmoid_prime(z):
-        return matrix.exp(-z) / ((1 + matrix.exp(-z)) ** 2)
+        return np.exp(-z) / ((1 + np.exp(-z)) ** 2)
 
     @staticmethod
     def to_vector(index):
-        e = matrix.zeros((10, 1))
+        e = np.zeros((10, 1))
         e[index] = 1.0
         return e
 
     @staticmethod
     def find_output(vector):
-        return matrix.argmax(vector)
+        return np.argmax(vector)
 
     def propagate(self, x):
         """Method used to get an input x go through the neuronal network and outputs a2"""
@@ -44,7 +44,7 @@ class NeuralNetwork:
         self.a.append(x)
 
         for i in range(0, self.size - 1):
-            self.z.append(matrix.dot(self.weights[i], self.a[i]) + self.bias[i])
+            self.z.append(np.dot(self.weights[i], self.a[i]) + self.bias[i])
             self.a.append(self.sigmoid(self.z[i]))
 
         self.z_s.append(self.z.copy())
@@ -59,9 +59,9 @@ class NeuralNetwork:
 
         error = [0] * (self.size - 1)
         y_hat = self.propagate(x)
-        error[-1] = matrix.multiply(-(y - y_hat), self.sigmoid_prime(self.z[-1]))
+        error[-1] = np.multiply(-(y - y_hat), self.sigmoid_prime(self.z[-1]))
         for i in range(1, self.size - 1):
-            error[-(i + 1)] = matrix.dot(self.weights[-i].T, error[-i]) * self.sigmoid_prime(self.z[-(i + 1)])
+            error[-(i + 1)] = np.dot(self.weights[-i].T, error[-i]) * self.sigmoid_prime(self.z[-(i + 1)])
 
         self.errors.append(error)
         self.a.clear()
@@ -78,9 +78,9 @@ class NeuralNetwork:
             self.bias[a] = self.bias[a] - tampon
 
         for b in range(0, self.size - 1):
-            tampon = matrix.dot(self.errors[0][b], matrix.transpose(self.a_s[0][b]))
+            tampon = np.dot(self.errors[0][b], np.transpose(self.a_s[0][b]))
             for i in range(1, len(self.errors)):
-                tampon = tampon + matrix.dot(self.errors[i][b], matrix.transpose(self.a_s[i][b]))
+                tampon = tampon + np.dot(self.errors[i][b], np.transpose(self.a_s[i][b]))
             tampon = tampon * self.learning_rate / m
             self.weights[b] = self.weights[b] - tampon
 
@@ -106,8 +106,8 @@ class NeuralNetwork:
             while h < len(exemples) - batch_size:
 
                 for g in range(h, h + batch_size):
-                    image = matrix.array(exemples[g].ex_input)
-                    self.back_propagation(matrix.reshape(image, (self.layers[0], 1)),
+                    image = np.array(exemples[g].ex_input)
+                    self.back_propagation(np.reshape(image, (self.layers[0], 1)),
                                           self.to_vector(exemples[g].ex_output))
 
                 self.gradient_descent(batch_size)
@@ -120,9 +120,9 @@ class NeuralNetwork:
         for v in range(0, len(test_input)):
 
             """Testing phase"""
-            image = matrix.array(test_input[v])
+            image = np.array(test_input[v])
 
-            if self.find_output(self.propagate(matrix.reshape(image, (self.layers[0], 1)))) == test_output[v]:
+            if self.find_output(self.propagate(np.reshape(image, (self.layers[0], 1)))) == test_output[v]:
                 count = count + 1
 
         print(
